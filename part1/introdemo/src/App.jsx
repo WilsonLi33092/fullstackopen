f65d33da47ec18d47e41d33b1d933d7ed80a057d
Wilsonli33092 <90936549+WilsonLi33092@users.noreply.github.com>
2025-01-11 13:11:11 -0500

    finished courseinfo

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,19 +1,37 @@
-const Hello = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+import Display from './Display'
+import Button from './Button'
+import History from './History'
+
 const App = () => {
-  const name = "Peter"
-  const age = 10
-  return (
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    console.log("left before", left)
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    console.log("updated left", updatedLeft)
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+    console.log('left after', left)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setTotal(left + updatedRight)
+    setRight(updatedRight)
+  }
+  return(
+    
     <div>
-      <h1>Greetings</h1>
-      <Hello name = "Maya" age = {26+10} />
-      <Hello name = {name} age={age} />
+      {left}
+      <Button onClick = {handleLeftClick} text = "plus"/>
+      <Button onClick = {handleRightClick} text = "reset"/>
+      {right}
+      <History allClicks = {allClicks}/>
 
     </div>
   )
