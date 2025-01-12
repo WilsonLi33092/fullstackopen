5797ff5a210883e16b6a9a208241187e9664e6cc
Wilsonli33092 <90936549+WilsonLi33092@users.noreply.github.com>
2025-01-12 00:13:00 -0500

    finished anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected,setSelected]=useState(0)
+  const [votes,setVote] = useState(new Array(anecdotes.length).fill(0))
+  const handleRandom =() => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
+  }
+  const handleVote = () => {
+    const newArray = [...votes]
+    newArray[selected] +=1
+    setVote(newArray)
+    console.log(Math.max(...votes))
+
+  }
+  const findMaxIndex = () => {
+    const arr = [...votes]
+    let max = 0;
+    let index = 0;
+    for(let i =0;i< arr.length;i++) {
+      if (arr[i] > max){
+        max = arr[i]
+        index = i;
+      }
+    }
+    return index
+  }
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick ={handleVote}>vote</button>
+      <button onClick = {handleRandom}>Random</button>
+      <p>{anecdotes[findMaxIndex()]}</p>
+    </div>
+  )
+}
+
+export default App
