46f4c0d96fc35fa686bd10bda8b1b8fb0bc34948
Wilsonli33092 <90936549+WilsonLi33092@users.noreply.github.com>
2025-01-13 13:52:09 -0500

    uibuild

--- part3/backend-node/index.js
+++ part3/backend-node/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const Note = require('./models/note')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -25,21 +27,26 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if(note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response,next) => {
+    Note.findById(request.params.id).then(note => {
+        if(note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -47,20 +54,40 @@ const generateId = () => {
     ? Math.max(...notes.map(n => Number(n.id))) : 0
     return String(maxId + 1)
 }
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response,next) => {
     const body = request.body
-    if(!body.content) {
-        return response.status(400).json({error: 'content missing'})
-    }
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
-    notes = notes.concat(note)
-    response.json(note)
+        important: body.important || false,
+    })
+    note.save()
+    .then(savedNote => {
+        response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
-const PORT = process.env.PORT || 3001
+app.put('/api/notes/:id', (request, response, next)=> {
+    const { content, important} = request.body
+    Note.findByIdAndUpdate(request.params.id, 
+        {content, important}, 
+        {new:true, runValidators: true, context: 'query'})
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
+
+})
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if( error.name === "ValidationError") {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
